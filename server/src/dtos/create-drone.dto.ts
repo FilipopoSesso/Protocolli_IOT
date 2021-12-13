194090f21a5284faa82815b1163a7f22949b356c
Francesco Berlaffa <f.berlaffa@gmail.com>
2021-12-13 18:39:29 +0100

    - fixed route to drones controller - added IsNotEmpty decorator to every field in the drone dto

--- server/src/dtos/create-drone.dto.ts
+++ server/src/dtos/create-drone.dto.ts
@@ -1,15 +1,19 @@
-import { IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateDroneDto {
+  @IsNotEmpty()
   @IsNumber()
   latitude: number;
 
+  @IsNotEmpty()
   @IsNumber()
   longitude: number;
 
+  @IsNotEmpty()
   @IsNumber()
   battery: number;
 
+  @IsNotEmpty()
   @IsNumber()
   altitude: number;
 }
