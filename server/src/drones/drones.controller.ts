00449d7aeedd3f9cdd7f8e0a98b5f0daa1e4cef1
Francesco Berlaffa <f.berlaffa@gmail.com>
2021-12-15 22:27:32 +0100

    Changed from express to fastify and added logging

--- server/src/drones/drones.controller.ts
+++ server/src/drones/drones.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Controller,
   Get,
+  Ip,
+  Logger,
   NotFoundException,
   Param,
   Post,
@@ -12,14 +14,17 @@ import { DronesService } from './drones.service';
 @Controller('v1/drones')
 export class DronesController {
   constructor(private dronesService: DronesService) {}
+  private readonly logger = new Logger(DronesController.name);
 
   @Get()
-  getAllDrones() {
+  getAllDrones(@Ip() ip: string) {
+    this.logger.log(`GET - /v1/drones - ${ip}`);
     return this.dronesService.findAll();
   }
 
   @Get('/:id')
-  async getDrone(@Param('id') id: string) {
+  async getDrone(@Param('id') id: string, @Ip() ip: string) {
+    this.logger.log(`GET - /v1/drones/${id} - ${ip}`);
     const drone = await this.dronesService.findOne(parseInt(id));
     if (!drone) {
       throw new NotFoundException('Drone not found');
@@ -28,12 +33,13 @@ export class DronesController {
   }
 
   @Post()
-  async createDrone(@Body() body: CreateDroneDto) {
-    const drone = await this.dronesService.create(
+  createDrone(@Body() body: CreateDroneDto, @Ip() ip: string) {
+    this.logger.log(`POST - /v1/drones - ${ip}`);
+    this.logger.log(body);
+    return this.dronesService.create(
       body.position,
       body.battery,
       body.altitude,
     );
-    return drone;
   }
 }
--- server/src/drones/drones.service.ts
+++ server/src/drones/drones.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Drone } from './drone.entity';
@@ -7,25 +7,21 @@ import { Drone } from './drone.entity';
 export class DronesService {
   constructor(@InjectRepository(Drone) private repo: Repository<Drone>) {}
 
-  create(
-    position: number[],
-    battery: number,
-    altitude: number,
-  ) {
-    const latitude = position[0];
-    const longitude = position[1];
-    const drone = this.repo.create({ latitude, longitude, battery, altitude });
+  create(position: number[], battery: number, altitude: number) {
+    const drone = this.repo.create({
+      latitude: position[0],
+      longitude: position[1],
+      battery,
+      altitude,
+    });
     return this.repo.save(drone);
   }
 
   findOne(id: number) {
-    if (!id) {
-      return null;
-    }
     return this.repo.findOne(id);
   }
 
-  findAll(){
-      return this.repo.find();
+  findAll() {
+    return this.repo.find();
   }
 }
