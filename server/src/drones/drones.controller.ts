f94f4f62fae51e1c1ac3eb067d672ec3e0de9be8
Francesco Berlaffa <francesco.berlaffa@stud.tecnicosuperiorekennedy.it>
2022-01-12 11:49:02 +0100

    htpp + mqtt

--- server/src/drones/drones.controller.ts
+++ server/src/drones/drones.controller.ts
@@ -16,8 +16,8 @@ import { DronesService } from './drones.service';
 import { UpdateDroneDto } from './dtos/update-drone.dto';
 import { ClientProxy, Ctx, MessagePattern, MqttContext, Payload } from '@nestjs/microservices';
 
-//@ApiTags('Drones')
-@Controller()
+@ApiTags('Drones')
+@Controller('v1/drones')
 export class DronesController {
   constructor(
     private dronesService: DronesService,
@@ -27,46 +27,46 @@ export class DronesController {
   }
   private readonly logger = new Logger(DronesController.name);
 
-  // @Get()
-  // getAllDrones(@Ip() ip: string) {
-  //   this.logger.log(`GET - /v1/drones - ${ip}`);
-  //   return this.dronesService.findAll();
-  // }
+  @Get()
+  getAllDrones(@Ip() ip: string) {
+    this.logger.log(`GET - /v1/drones - ${ip}`);
+    return this.dronesService.findAll();
+  }
 
-  // @Get('/:id')
-  // async getDrone(@Param('id') id: string, @Ip() ip: string) {
-  //   this.logger.log(`GET - /v1/drones/${id} - ${ip}`);
-  //   const drone = await this.dronesService.findOne(id);
-  //   return drone;
-  // }
+  @Get('/:id')
+  async getDrone(@Param('id') id: string, @Ip() ip: string) {
+    this.logger.log(`GET - /v1/drones/${id} - ${ip}`);
+    const drone = await this.dronesService.findOne(id);
+    return drone;
+  }
 
-  // @Get('/:id/status')
-  // async getLastDroneStatus(@Param('id') id: string, @Ip() ip: string) {
-  //   this.logger.log(`GET - /v1/drones/${id}/status - ${ip}`);
-  //   const lastStatus = await this.dronesService.findLast(id);
-  //   return lastStatus;
-  // }
+  @Get('/:id/status')
+  async getLastDroneStatus(@Param('id') id: string, @Ip() ip: string) {
+    this.logger.log(`GET - /v1/drones/${id}/status - ${ip}`);
+    const lastStatus = await this.dronesService.findLast(id);
+    return lastStatus;
+  }
 
-  // @Post('/new')
-  // createDrone(@Body() body: CreateDroneDto, @Ip() ip: string) {
-  //   this.logger.log(`POST - /v1/drones/new - ${ip}`);
-  //   this.logger.log(body);
-  //   return this.dronesService.create(body);
-  // }
+  @Post('/new')
+  createDrone(@Body() body: CreateDroneDto, @Ip() ip: string) {
+    this.logger.log(`POST - /v1/drones/new - ${ip}`);
+    this.logger.log(body);
+    return this.dronesService.create(body);
+  }
 
-  // @Patch('/:id')
-  // updateDrone(
-  //   @Param('id') id: string,
-  //   @Body() body: UpdateDroneDto,
-  //   @Ip() ip: string,
-  // ) {
-  //   this.logger.log(`Patch - /v1/drones/${id} - ${ip}`);
-  //   this.logger.log(body);
-  //   return this.dronesService.update(id, body);
-  // }
+  @Patch('/:id')
+  updateDrone(
+    @Param('id') id: string,
+    @Body() body: UpdateDroneDto,
+    @Ip() ip: string,
+  ) {
+    this.logger.log(`Patch - /v1/drones/${id} - ${ip}`);
+    this.logger.log(body);
+    return this.dronesService.update(id, body);
+  }
 
   @MessagePattern('v1/drones/+/data/all')
-  saveDroneStatus(@Payload() data, @Ctx() context: MqttContext) {
+  saveDroneStatus(@Payload() data: SaveDroneStatusDto, @Ctx() context: MqttContext) {
     this.logger.log(data);
     this.dronesService.saveStatus(
       data.position,
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,39 +1,41 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-// import {
-//   FastifyAdapter,
-//   NestFastifyApplication,
-// } from '@nestjs/platform-fastify';
-//import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    {
-      transport: Transport.MQTT,
-      options: {
-        url: 'mqtt://test.mosquitto.org:1883'
-      },
-    },
+    new FastifyAdapter(),
   );
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.MQTT,
+    options: {
+      url: 'mqtt://test.mosquitto.org:1883',
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, //this parameter removes all the invalid properties from the request
     }),
   );
 
-  // const config = new DocumentBuilder()
-  // .setTitle('Protocolli IoT')
-  // .setDescription('Drones API')
-  // .setVersion('1.0')
-  // .addTag('Drones')
-  // .build();
+  const config = new DocumentBuilder()
+    .setTitle('Protocolli IoT')
+    .setDescription('Drones API')
+    .setVersion('1.0')
+    .addTag('Drones')
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-  await app.listen();
+  await app.startAllMicroservices();
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
